ab789c0cd47c86073f9b5cc562d1ed8548990e7f
Calvin Webster <calweb@gmail.com>
2015-11-05 16:13:52 -0500

    moar code

--- client/app.js
+++ client/app.js
@@ -1,3 +1,33 @@
 (function () {
-  $(document).ready(function () {});
+  $(document).ready(function () {
+    page.init();
+  });
+  var page = {
+    init: function () {
+      page.initStyling();
+      page.initEvents();
+    },
+    initStyling: function () {
+      page.getDate();
+    },
+    initEvents: function () {
+      $('.noLikey').on('click', function (event) {
+        page.getDate();
+      });
+    },
+    loadTmpl: function ($el, data, template) {
+      var tmpl = _.template(template);
+      $el.html(tmpl(data));
+    },
+    getDate: function () {
+      $.ajax({
+        url: '/random',
+        method: 'GET',
+        success: function (data) {
+          window.dateData = JSON.parse(data);
+          page.loadTmpl($('.dateDiv'), dateData, $('#dateTmpl').html());
+        }
+      });
+    }
+  };
 })();
